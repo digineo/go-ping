7d6f3649cf55a2de3016619866241744c97cd709
Dominik Menke <dom@digineo.de>
2018-02-22 21:33:38 +0100

    cmd/multiping: group statistical data in a struct

--- cmd/multiping/destination.go
+++ cmd/multiping/destination.go
@@ -24,6 +24,17 @@ type destination struct {
 	*history
 }
 
+type stat struct {
+	pktSent   int
+	pktLoss   float64
+	last      time.Duration
+	best      time.Duration
+	worst     time.Duration
+	mean      time.Duration
+	stddev    time.Duration
+	lastError string
+}
+
 func (u *destination) ping(pinger *ping.Pinger) {
 	u.addResult(pinger.PingRTT(u.remote))
 }
@@ -40,19 +51,19 @@ func (s *history) addResult(rtt time.Duration, err error) {
 	s.mtx.Unlock()
 }
 
-func (s *history) compute() (pktSent int, pktLoss float64, last, best, worst, mean, stddev time.Duration) {
+func (s *history) compute() (st stat) {
 	s.mtx.RLock()
 	defer s.mtx.RUnlock()
 
 	if s.received == 0 {
 		if s.lost > 0 {
-			pktLoss = 1.0
+			st.pktLoss = 1.0
 		}
 		return
 	}
 
 	collection := s.results[:]
-	pktSent = s.received + s.lost
+	st.pktSent = s.received + s.lost
 	size := len(s.results)
 
 	if s.received < size {
@@ -60,28 +71,28 @@ func (s *history) compute() (pktSent int, pktLoss float64, last, best, worst, me
 		size = s.received
 	}
 
-	last = collection[s.received%size]
-	pktLoss = float64(s.lost) / float64(size)
-	best, worst = collection[0], collection[0]
+	st.last = collection[s.received%size]
+	st.pktLoss = float64(s.lost) / float64(size)
+	st.best, st.worst = collection[0], collection[0]
 
 	total := time.Duration(0)
 	for _, rtt := range collection {
-		if rtt < best {
-			best = rtt
+		if rtt < st.best {
+			st.best = rtt
 		}
-		if rtt > worst {
-			worst = rtt
+		if rtt > st.worst {
+			st.worst = rtt
 		}
 		total += rtt
 	}
 
-	mean = time.Duration(float64(total) / float64(s.received))
+	st.mean = time.Duration(float64(total) / float64(s.received))
 
 	stddevNum := float64(0)
 	for _, rtt := range collection {
-		stddevNum += math.Pow(float64(rtt-mean), 2)
+		stddevNum += math.Pow(float64(rtt-st.mean), 2)
 	}
-	stddev = time.Duration(math.Sqrt(stddevNum / float64(mean)))
+	st.stddev = time.Duration(math.Sqrt(stddevNum / float64(st.mean)))
 
 	return
 }
