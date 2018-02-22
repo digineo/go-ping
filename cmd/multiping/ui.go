bb9993c8287c4bfde8151b225e51bce248be7831
Dominik Menke <dom@digineo.de>
2018-02-22 18:54:29 +0100

    cmd/multiping: seperate host and address

--- cmd/multiping/ui.go
+++ cmd/multiping/ui.go
@@ -24,15 +24,16 @@ func buildTUI(destinations []*destination) *userInterface {
 
 	ui.table.SetTitle(" multiping (press [q] to exit) ")
 
-	ui.table.SetCell(0, 0, tview.NewTableCell(fmt.Sprintf("%-60s", "Host")).SetAlign(tview.AlignLeft))
-	ui.table.SetCell(0, 1, tview.NewTableCell("sent").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 2, tview.NewTableCell("loss").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 3, tview.NewTableCell("last").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 4, tview.NewTableCell("best").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 5, tview.NewTableCell("worst").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 6, tview.NewTableCell("mean").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 7, tview.NewTableCell("stddev").SetAlign(tview.AlignRight))
-	ui.table.SetCell(0, 8, tview.NewTableCell("last err").SetAlign(tview.AlignLeft))
+	ui.table.SetCell(0, 0, tview.NewTableCell("host").SetAlign(tview.AlignLeft))
+	ui.table.SetCell(0, 1, tview.NewTableCell("address").SetAlign(tview.AlignLeft))
+	ui.table.SetCell(0, 2, tview.NewTableCell("sent").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 3, tview.NewTableCell("loss").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 4, tview.NewTableCell("last").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 5, tview.NewTableCell("best").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 6, tview.NewTableCell("worst").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 7, tview.NewTableCell("mean").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 8, tview.NewTableCell("stddev").SetAlign(tview.AlignRight))
+	ui.table.SetCell(0, 9, tview.NewTableCell("last err").SetAlign(tview.AlignLeft))
 
 	ui.app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
@@ -48,14 +49,16 @@ func buildTUI(destinations []*destination) *userInterface {
 		return event
 	})
 
-	cols := 9
+	cols := 10
 	for r, u := range destinations {
 		for c := 0; c < cols; c++ {
 			var cell *tview.TableCell
 			switch c {
 			case 0:
-				cell = tview.NewTableCell(u.display).SetAlign(tview.AlignLeft)
-			case 8:
+				cell = tview.NewTableCell(u.host).SetAlign(tview.AlignLeft)
+			case 1:
+				cell = tview.NewTableCell(u.remote.IP.String()).SetAlign(tview.AlignLeft)
+			case 9:
 				cell = tview.NewTableCell("").SetAlign(tview.AlignLeft)
 			default:
 				cell = tview.NewTableCell("n/a").SetAlign(tview.AlignRight)
@@ -79,13 +82,13 @@ func (ui *userInterface) update(interval time.Duration) {
 			sent, loss, last, best, worst, mean, stddev := u.compute()
 			r := i + 2
 
-			ui.table.GetCell(r, 1).SetText(strconv.Itoa(sent))
-			ui.table.GetCell(r, 2).SetText(fmt.Sprintf("%0.2f%%", loss))
-			ui.table.GetCell(r, 3).SetText(ts(last))
-			ui.table.GetCell(r, 4).SetText(ts(best))
-			ui.table.GetCell(r, 5).SetText(ts(worst))
-			ui.table.GetCell(r, 6).SetText(ts(mean))
-			ui.table.GetCell(r, 7).SetText(stddev.String())
+			ui.table.GetCell(r, 2).SetText(strconv.Itoa(sent))
+			ui.table.GetCell(r, 3).SetText(fmt.Sprintf("%0.2f%%", loss))
+			ui.table.GetCell(r, 4).SetText(ts(last))
+			ui.table.GetCell(r, 5).SetText(ts(best))
+			ui.table.GetCell(r, 6).SetText(ts(worst))
+			ui.table.GetCell(r, 7).SetText(ts(mean))
+			ui.table.GetCell(r, 8).SetText(stddev.String())
 
 			if u.lastErr != nil {
 				ui.table.GetCell(r, 8).SetText(fmt.Sprintf("%v", u.lastErr))
