22b99772d1194f82e58d1bd9e263622948a3c631
Julian Kornberger <jk+github@digineo.de>
2019-01-23 01:01:01 +0100

    Increase test coverage

---
+++ pinger_test.go
@@ -0,0 +1,26 @@
+package ping
+
+import (
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPinger(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	pinger, err := New("0.0.0.0", "::")
+	require.NoError(err)
+	require.NotNil(pinger)
+	defer pinger.Close()
+
+	for _, target := range []string{"127.0.0.1", "::1"} {
+		rtt, err := pinger.PingAttempts(&net.IPAddr{IP: net.ParseIP(target)}, time.Second, 2)
+		assert.NoError(err, target)
+		assert.NotZero(rtt, target)
+	}
+}
