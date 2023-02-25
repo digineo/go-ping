e38afe6129d20ff38be0d8ff85a29f0e88573010
Dominik Menke <dom@digineo.de>
2023-02-26 00:22:11 +0100

    update dependencies

--- payload.go
+++ payload.go
@@ -13,11 +13,10 @@ var (
 	// SetLogger allows updating the Logger. For details, see
 	// "github.com/digineo/go-logwrap".Instance.SetLogger.
 	SetLogger = log.SetLogger
-)
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
+	// SA1019: rand.Seed has been deprecated, provide package-local RNG
+	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+)
 
 // Payload represents additional data appended to outgoing ICMP Echo
 // Requests.
@@ -26,7 +25,7 @@ type Payload []byte
 // Resize will assign a new payload of the given size to p.
 func (p *Payload) Resize(size uint16) {
 	buf := make([]byte, size)
-	if _, err := rand.Read(buf); err != nil {
+	if _, err := rng.Read(buf); err != nil {
 		log.Errorf("error resizing payload: %v", err)
 		return
 	}
